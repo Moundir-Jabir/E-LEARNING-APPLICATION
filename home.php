64f678f29b023834a97268d718ac0f2d74db3b1c
Moundir Jabir <elmondhirjabir@gmail.com>
2022-01-15 01:41:58 +0100

    page home

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="home.css">
+    <title>Home</title>
+</head>
+<body>
+    <?php
+        include 'navbar.php';
+    ?>
+    <main>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div id="student" class="col-lg-2 col-md-3 col-sm-5 col-11 case">
+                    <img src="image/1.svg" alt="student">
+                    <p>Students</p>
+                    <h2>243</h2>
+                </div>
+                <div id="course" class="col-lg-2 col-md-3 col-sm-5 col-11 case">
+                    <img src="image/2.svg" alt="course">
+                    <p>Course</p>
+                    <h2>13</h2>
+                </div>
+                <div id="payment" class="col-lg-2 col-md-3 col-sm-5 col-11 case">
+                    <img src="image/3.svg" alt="payments">
+                    <p>Payments</p>
+                    <h2>556,00</h2>
+                </div>
+                <div id="user" class="col-lg-2 col-md-3 col-sm-5 col-11 case">
+                    <img src="image/4.svg" alt="user">
+                    <p>Users</p>
+                    <h2>3</h2>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,67 @@
+<div id="jaune">
+    <div id="titre">
+        <h1>E-classe</h1>
+    </div>
+    <div>
+        <img src="image/youcode.png" alt="youcode" id="youcode" class="img-fluid">
+        <p id="name">Admin name</p>
+        <p id="admin">Admin</p>
+    </div>
+    <nav>
+        <div>
+            <a href="">
+                <img src="image/Vector.svg" alt="home">
+                <span>Home</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <img src="image/Vector-2.svg" alt="course">
+                <span>Course</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <img src="image/Vector-3.svg" alt="students">
+                <span>Students</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <img src="image/Vector-7.svg" alt="payment">
+                <span>Payment</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <img src="image/Vector-4.svg" alt="report">
+                <span>Report</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <img src="image/Vector-5.svg" alt="settings">
+                <span>Settings</span>
+            </a>
+        </div>
+    </nav>
+    <div id="logout">
+        <a href="">
+            <span>Logout</span>
+            <img src="image/Vector-6.svg" alt="logout">
+        </a>
+    </div>
+</div>
+<div id="menu">
+    <div>
+        <img src="image/fleche.svg" alt="fleche">
+    </div>
+    <div id="formulaire">
+        <form>
+            <input type="search" class="form-control" placeholder="Search...">
+        </form>
+        <div>
+            <img src="image/horloge.svg" alt="cloche">
+        </div>
+    </div>
+</div>
