651eb6a557922f7760f9bef994bf98a05800c2a4
Moundir Jabir <elmondhirjabir@gmail.com>
2022-01-31 10:58:16 +0100

    probleme responsivité button add student

--- assets/students.php
+++ assets/students.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/3514a06926.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/students.css">
     <title>Students</title>
@@ -31,7 +32,7 @@
             </div>
             <div>
                 <img src="image/fleches.svg" alt="deux-fleche">
-                <button>ADD NEW STUDENT</button>
+                <button><span id="add">ADD NEW STUDENT</span><i id="plus" class="fas fa-user-plus"></i></button>
             </div>
         </div> <hr>
         <div class="table-responsive">
